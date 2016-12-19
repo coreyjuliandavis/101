22afe4bdb431fede6026f6f4980e5cc09bccdb45
coreyjuliandavis <coreyjuliandavis@gmail.com>
2016-12-18 19:32:20 -0500

    updating

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,14 +1,27 @@
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = %w(rock paper scissors lizard spock)
+
+def prompt(message)
+  puts("=> #{message}")
+end
 
 def win?(first, second)
   (first == "rock" && second == "scissors") ||
     (first == "paper" && second == "rock") ||
-    (first == "scissors" && second == "paper")
+    (first == "scissors" && second == "paper") ||
+    (first == "rock" && second == "lizard") ||
+    (first == "lizard" && second == "spock") ||
+    (first == "spock" && second == "scissors") ||
+    (first == "scissors" && second == "lizard") ||
+    (first == "lizard" && second == "paper") ||
+    (first == "paper" && second == "spock") ||
+    (first == "spock" && second == "rock")
 end
+
+# (l) (r) (p) (sp) (sc)
 
 def display_result(player, computer)
   if win?(player, computer)
-    prompt("You won")
+    prompt("You won!")
   elsif win?(computer, player)
     prompt("Computer won!")
   else
@@ -16,9 +29,6 @@ def display_result(player, computer)
   end
 end
 
-def prompt(message)
-  puts("=> #{message}")
-end
 
 loop do
   choice = ''
@@ -38,6 +48,7 @@ loop do
   prompt("You chose #{choice}, the computer chose #{computer_choice}")
 
   display_result(choice, computer_choice)
+
 
   prompt("Do you want to play again?")
   answer = gets.chomp
